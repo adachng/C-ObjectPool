8644fb585879718a560c3153d1c46c24192d6d21
adachng <11900409+adachng@users.noreply.github.com>
2026-06-18 04:52:53 +0800

    docs: use `Breathe` in `conf.py`

--- doc/conf.py
+++ doc/conf.py
@@ -3,25 +3,29 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'C-ObjectPool'
-copyright = '2026, adachng'
-author = 'adachng'
+project = "C-ObjectPool"
+copyright = "2026, adachng"
+author = "adachng"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+sys.path.append(os.path.abspath("breathe"))
+extensions = ["breathe"]
+breathe_projects = {project: "xml"}
+breathe_default_project = project
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
